5b75c7658784f4d78cc0418ee33353837e8b2bfd
Srinivas Manya <srinivasmanya2000@gmail.com>
2026-03-10 16:39:36 +0530

    Render deployment ready

--- hbooking/settings.py
+++ hbooking/settings.py
@@ -57,11 +57,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hbooking.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.User'
